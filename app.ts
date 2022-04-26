d6bc1335f8ad4e826a9ea87c4b316c9850d8ed20
Jonathan Poelman <jonathan.poelman@protonmail.com>
2022-04-26 14:12:44 +0200

    implemented initial version of consumer microservice which listens to an ldes streams and adds the resources to a virtuoso triplestore

---
+++ app.ts
@@ -0,0 +1,182 @@
+import { newEngine } from "@treecg/actor-init-ldes-client";
+import * as RDFJS from "rdf-js";
+import { query } from "mu";
+import { Quad, DataFactory } from "n3";
+import { example, ldes, prov } from "./namespaces";
+import SC2 from "sparql-client-2";
+
+const { SparqlClient, SPARQL } = SC2;
+
+const SPARQL_CLIENT = new SparqlClient(process.env.MU_SPARQL_ENDPOINT, {
+	requestDefaults: { headers: { "mu-auth-sudo": true } },
+});
+
+const { quad, namedNode, variable } = DataFactory;
+
+const BASEURI = "http://ldes-time-fragmenter:3000";
+
+const stream = example("post-stream");
+
+let lastInsertedMember: any;
+
+function toString(term: RDFJS.Term) {
+	switch (term.termType) {
+		case "NamedNode":
+			return `<${term.value}>`;
+		case "Literal":
+			return `"${term.value}"`;
+		case "Quad":
+			return `${toString(term.subject)} ${toString(
+				term.predicate
+			)} ${toString(term.object)}.`;
+		case "Variable":
+			return `?${term.value}`;
+		default:
+			return term.value;
+	}
+}
+
+async function executeInsertQuery(quads: RDFJS.Quad[]) {
+	let triplesString = "";
+	quads.forEach((quadObj) => {
+		triplesString += toString(quadObj) + "\n";
+	});
+	const sparql_query = `
+    INSERT DATA {
+        GRAPH <${process.env.MU_APPLICATION_GRAPH}> {
+            ${triplesString}
+        }
+    }
+  `;
+	try {
+		const response = await SPARQL_CLIENT.query(sparql_query).execute();
+		console.log(response);
+	} catch (e) {
+		console.error(e);
+	}
+}
+
+async function executeDeleteQuery(quads: RDFJS.Quad[]) {
+	let triplesString = "";
+	quads.forEach((quadObj) => {
+		triplesString += toString(quadObj) + "\n";
+	});
+	const sparql_query = `
+    DELETE {
+      GRAPH <${process.env.MU_APPLICATION_GRAPH}> {
+            ${triplesString}
+      }
+    } WHERE {
+        GRAPH <${process.env.MU_APPLICATION_GRAPH}> {
+            ${triplesString}
+        }
+    }
+  `;
+	console.log(sparql_query);
+	try {
+		await SPARQL_CLIENT.query(sparql_query).execute();
+	} catch (e) {
+		console.error(e);
+	}
+}
+
+async function getEndTime() {
+	const sparql_query = `
+    SELECT ?t where {
+      GRAPH <${process.env.MU_APPLICATION_GRAPH}> {
+        ${toString(stream)} ${toString(prov("endedAtTime"))} ?t.
+      }
+    }
+  `;
+
+	try {
+		const response = await SPARQL_CLIENT.query(sparql_query).execute();
+		console.log(response);
+		if (response.bindings && response.bindings.length) {
+			const timeString = response.bindings[0].t.value.split("^^")[0];
+			let time: Date = new Date(timeString);
+			time.setMilliseconds(time.getMilliseconds() + 1);
+
+			return time;
+		}
+		return;
+	} catch (e) {
+		console.error(e);
+	}
+}
+
+async function main() {
+	try {
+		const endTime = await getEndTime();
+		console.log(endTime);
+		let url = `${BASEURI}/social-media-posts/1`;
+		let options = {
+			pollingInterval: 500, // millis
+			representation: "Quads", //Object or Quads
+			fromTime: endTime,
+			mimeType: "application/ld+json",
+			emitMemberOnce: true,
+			jsonLdContext: {
+				//Only necessary for Object representation
+				"@context": [
+					{
+						"@base": BASEURI,
+						prov: prov().value,
+					},
+				],
+			},
+		};
+		let LDESClient = new newEngine();
+		let eventstreamSync = LDESClient.createReadStream(url, options);
+
+		// const timeoutms = 2000; // amount of milliseconds before timeout
+		// const timeout = setTimeout(() => eventstreamSync.pause(), timeoutms);
+		eventstreamSync.on("data", (member) => {
+			if (options.representation && options.representation === "Quads") {
+				lastInsertedMember = member;
+				const memberURI = member.id.value;
+				console.log("Member:", memberURI);
+				const quads = member.quads.filter(
+					(quadObj) => quadObj.subject.value === member.id.value
+				);
+				executeInsertQuery(quads);
+			}
+
+			// Want to pause event stream?
+			// eventstreamSync.pause();
+		});
+
+		eventstreamSync.on("pause", async () => {
+			console.log("PAUSE");
+			if (lastInsertedMember) {
+				console.log(lastInsertedMember.quads[1].object);
+				const timeStamp = lastInsertedMember.quads.find(
+					(quadObj) =>
+						quadObj.predicate.value ===
+						prov("generatedAtTime").value
+				);
+				if (timeStamp) {
+					const genericTimeQuad: Quad = quad(
+						stream,
+						prov("endedAtTime"),
+						variable("t")
+					);
+					await executeDeleteQuery([genericTimeQuad]);
+					const newTimeQuad: Quad = quad(
+						stream,
+						prov("endedAtTime"),
+						timeStamp.object
+					);
+					await executeInsertQuery([newTimeQuad]);
+				}
+			}
+		});
+		eventstreamSync.on("end", () => {
+			console.log("No more data!");
+		});
+	} catch (e) {
+		console.error(e);
+	}
+}
+
+main();
---
+++ namespaces.ts
@@ -0,0 +1,9 @@
+import namespace from "@rdfjs/namespace";
+
+export const ldes = namespace(
+	"http://mu.semte.ch/services/ldes-time-fragmenter/"
+);
+
+export const prov = namespace("http://www.w3.org/ns/prov#");
+
+export const example = namespace("http://example.org/");
