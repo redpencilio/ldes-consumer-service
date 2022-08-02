73a4467c8473dff85985d43e7c17bed0c1fe7bf3
Elena Poelman <elena.poelman@redpencil.io>
2022-08-02 15:32:20 +0200

    update config

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
 export const LDES_ENDPOINT_VIEW = process.env.LDES_ENDPOINT_VIEW;
 export const LDES_POLLING_INTERVAL = parseInt(process.env.LDES_POLLING_INTERVAL ?? "500");
 export const LDES_STREAM = process.env.LDES_STREAM || "http://example.org/example-stream";
-export const REPLACE_VERSIONS = process.env.REPLACE_VERSIONS;
+export const REPLACE_VERSIONS = process.env.REPLACE_VERSIONS === "false" ? false : true;
 export const LDES_RELATION_PATH = process.env.LDES_RELATION_PATH || "http://www.w3.org/ns/prov#generatedAtTime";
 export const MU_APPLICATION_GRAPH = process.env.MU_APPLICATION_GRAPH;
 export const CRON_PATTERN = process.env.CRON_PATTERN || "* 0 * * * *";
