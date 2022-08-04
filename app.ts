ad5c8aa647e141ea6644bc0d04f6b094890ec162
Elena Poelman <elena.poelman@redpencil.io>
2022-08-04 16:44:39 +0200

    untabify

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 import { PURL } from "./namespaces";
 import {
-	executeDeleteInsertQuery,
-	fetchState,
+  executeDeleteInsertQuery,
+  fetchState,
   updateState,
 } from "./sparql-queries";
 
@@ -14,39 +14,39 @@ import { CRON_PATTERN, LDES_ENDPOINT_VIEW, LDES_POLLING_INTERVAL, REPLACE_VERSIO
 const { quad, variable } = DataFactory;
 
 async function processMember(member: Member) {
-	const quadsToAdd: RDF.Quad[] = member.quads;
-	const quadsToRemove: RDF.Quad[] = [];
-	const baseResourceUri = extractBaseResourceUri(member);
-	if (baseResourceUri && REPLACE_VERSIONS) {
-		quadsToRemove.push(
-			quad(variable("s"), PURL("isVersionOf"), baseResourceUri)
-		);
+  const quadsToAdd: RDF.Quad[] = member.quads;
+  const quadsToRemove: RDF.Quad[] = [];
+  const baseResourceUri = extractBaseResourceUri(member);
+  if (baseResourceUri && REPLACE_VERSIONS) {
+    quadsToRemove.push(
+      quad(variable("s"), PURL("isVersionOf"), baseResourceUri)
+    );
     quadsToRemove.push(quad(variable("s"), variable("p"), variable("o")))
-	}
-	await executeDeleteInsertQuery(quadsToRemove, quadsToAdd);
+  }
+  await executeDeleteInsertQuery(quadsToRemove, quadsToAdd);
 }
 
 const consumerJob = new CronJob(CRON_PATTERN, async () => {
-	try {
-		const initialState = await fetchState();
-		const endpoint = LDES_ENDPOINT_VIEW;
-		if (endpoint) {
-			const consumer = new Consumer({
-				endpoint,
-				interval: LDES_POLLING_INTERVAL,
-				initialState,
-			});
-			consumer.listen(async (member, state) => {
-				convertBlankNodes(member.quads); 
-				await processMember(member);
+  try {
+    const initialState = await fetchState();
+    const endpoint = LDES_ENDPOINT_VIEW;
+    if (endpoint) {
+      const consumer = new Consumer({
+        endpoint,
+        interval: LDES_POLLING_INTERVAL,
+        initialState,
+      });
+      consumer.listen(async (member, state) => {
+        convertBlankNodes(member.quads); 
+        await processMember(member);
         await updateState(state);
-			});
-		} else {
-			throw new Error("No endpoint provided");
-		}
-	} catch (e) {
-		console.error(e); 
-	}
+      });
+    } else {
+      throw new Error("No endpoint provided");
+    }
+  } catch (e) {
+    console.error(e); 
+  }
 })
 
 consumerJob.start();
--- utils.ts
+++ utils.ts
@@ -9,70 +9,70 @@ import { LDES_RELATION_PATH } from "./config";
 import { sparqlEscapeString, sparqlEscapeUri } from 'mu';
 
 export function toString(term: Term): string {
-	switch (term.termType) {
-		case "NamedNode":
-			return sparqlEscapeUri(term.value);
-		case "Literal":
-			let result = sparqlEscapeString(term.value);
-			
-			if(term.language)
-      	result += `@${term.language}`;
-			if(term.datatype)
-				result += `^^${sparqlEscapeUri(term.datatype.value)}`
-			return result;
-		case "Quad":
-			return `${toString(term.subject)} ${toString(
-				term.predicate
-			)} ${toString(term.object)}.`;
-		case "Variable":
-			return `?${term.value}`;
-		default:
-			return term.value;
-	}
+  switch (term.termType) {
+    case "NamedNode":
+      return sparqlEscapeUri(term.value);
+    case "Literal":
+      let result = sparqlEscapeString(term.value);
+      
+      if(term.language)
+        result += `@${term.language}`;
+      if(term.datatype)
+        result += `^^${sparqlEscapeUri(term.datatype.value)}`
+      return result;
+    case "Quad":
+      return `${toString(term.subject)} ${toString(
+        term.predicate
+      )} ${toString(term.object)}.`;
+    case "Variable":
+      return `?${term.value}`;
+    default:
+      return term.value;
+  }
 }
 
 export function extractTimeStamp(member: Member): Date {
-	const timeStamp: RDF.Quad = member.quads.find(
-		(quadObj) => quadObj.predicate.value === LDES_RELATION_PATH
-	)!;
-	return toDate(timeStamp.object as RDF.Literal);
+  const timeStamp: RDF.Quad = member.quads.find(
+    (quadObj) => quadObj.predicate.value === LDES_RELATION_PATH
+  )!;
+  return toDate(timeStamp.object as RDF.Literal);
 }
 
 export function toDate(node: RDF.Literal): Date {
-	const timeString = node.value.split("^^")[0];
-	return new Date(timeString);
+  const timeString = node.value.split("^^")[0];
+  return new Date(timeString);
 }
 
 export function fromDate(date: Date): RDF.Literal {
-	return literal(date.toISOString(), XSD("dateTime"));
+  return literal(date.toISOString(), XSD("dateTime"));
 }
 
 export function convertBlankNodes(quads: RDF.Quad[]) {
-	const blankNodesMap = new Map<RDF.BlankNode, RDF.NamedNode>();
-	quads.forEach((quad) => {
-		if (quad.subject.termType === "BlankNode") {
-			if (!blankNodesMap.has(quad.subject)) {
-				blankNodesMap.set(quad.subject, BLANK(uuidv4()));
-			}
-			quad.subject = blankNodesMap.get(quad.subject)!;
-		}
-		if (quad.object.termType === "BlankNode") {
-			if (!blankNodesMap.has(quad.object)) {
-				blankNodesMap.set(quad.object, BLANK(uuidv4()));
-			}
-			quad.object = blankNodesMap.get(quad.object)!;
-		}
-	});
+  const blankNodesMap = new Map<RDF.BlankNode, RDF.NamedNode>();
+  quads.forEach((quad) => {
+    if (quad.subject.termType === "BlankNode") {
+      if (!blankNodesMap.has(quad.subject)) {
+        blankNodesMap.set(quad.subject, BLANK(uuidv4()));
+      }
+      quad.subject = blankNodesMap.get(quad.subject)!;
+    }
+    if (quad.object.termType === "BlankNode") {
+      if (!blankNodesMap.has(quad.object)) {
+        blankNodesMap.set(quad.object, BLANK(uuidv4()));
+      }
+      quad.object = blankNodesMap.get(quad.object)!;
+    }
+  });
 }
 
 export function extractBaseResourceUri(
-	member: Member
+  member: Member
 ): RDF.NamedNode | undefined {
-	const baseResourceMatches = member.quads.filter((quadObj) =>
-		quadObj.predicate.equals(PURL("isVersionOf"))
-	);
-	if (baseResourceMatches && baseResourceMatches.length) {
-		return baseResourceMatches[0].object as RDF.NamedNode;
-	}
-	return;
+  const baseResourceMatches = member.quads.filter((quadObj) =>
+    quadObj.predicate.equals(PURL("isVersionOf"))
+  );
+  if (baseResourceMatches && baseResourceMatches.length) {
+    return baseResourceMatches[0].object as RDF.NamedNode;
+  }
+  return;
 }
