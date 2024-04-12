2fe28a6f6c97d6ce8def19079b7fefabb937cdba
gauquiebart <bart.gauquie@gmail.com>
2024-04-12 11:13:55 +0200

    LPDC-1103: 1/ Upgrade to v4.0.13 for actor-init-ldes-client (fixes stream resume) 2/ reduce  high water mark to 100

--- member-processor.ts
+++ member-processor.ts
@@ -28,10 +28,7 @@ export default class MemberProcessor extends Writable {
   membersToProcess: MemberWithCallBack[] = [];
 
   constructor () {
-    super({ objectMode: true, highWaterMark: 100000 }); //TODO LPDC-1103: it works to set it higher, but we should here listen to events ... and when the high water mark gets called
-    // -> log that in a variable -> and then when the membersToProcess < 100 => call resume again ...
-    // or maybe we are not correctly offloading ... meaning that the stream thinks that the numbers of items in the stream are just mounting, even if we _write() them ...
-    //
+    super({ objectMode: true, highWaterMark: 100 });
     this.treeProperties = {
       versionOfPath: LDES_VERSION_OF_PATH,
       timestampPath: LDES_TIMESTAMP_PATH
@@ -40,11 +37,8 @@ export default class MemberProcessor extends Writable {
   }
 
   _write (member: Member, _encoding : string, callback: () => void) {
-    //TODO LPDC-1103: add more logging here ?
     if (member.id) {
       this.membersToProcess.push({ member, callback });
-    } else {
-      console.log(member);
     }
     return true;
   }
