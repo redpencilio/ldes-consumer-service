d92df4ee4801ba940b19c2955708c7b4ef8f193a
Sergio Fenoll <sergio@fenoll.be>
2024-11-08 14:52:40 +0100

    chore: move files to lib folder

---
+++ cfg.ts
@@ -0,0 +1,65 @@
+import env from "env-var";
+import { getLoggerFor } from "./lib/logger";
+
+const logger = getLoggerFor("config");
+
+export const LDES_ENDPOINT_VIEW = env.get("LDES_ENDPOINT_VIEW").required().asString();
+export const LDES_POLLING_INTERVAL = env.get("LDES_POLLING_INTERVAL").default(60000).asIntPositive();
+export const LDES_REQUESTS_PER_MINUTE = env.get("LDES_REQUESTS_PER_MINUTE").default(0).asIntPositive();
+export const LDES_ENDPOINT_HEADERS_STRING = env.get("LDES_ENDPOINT_HEADERS").default("{}").asString();
+export let LDES_ENDPOINT_HEADERS = {};
+
+try {
+  LDES_ENDPOINT_HEADERS = JSON.parse(LDES_ENDPOINT_HEADERS_STRING);
+} catch (e: any) {
+  logger.error(`Failed to parse contents of LDES_HEADERS. Faulty content: ${LDES_ENDPOINT_HEADERS_STRING}`);
+  logger.error(e);
+  throw e;
+}
+
+
+export const INGEST_MODE = env.get("INGEST_MODE").default("ALL").asEnum(["ALL", "MATERIALIZE"]);
+export const REPLACE_VERSIONS = env.get("REPLACE_VERSIONS").default("true").asBool();
+export const PERSIST_STATE = env.get("PERSIST_STATE").default("false").asBool()
+
+export const SPARQL_ENDPOINT_HEADER_PREFIX = "SPARQL_ENDPOINT_HEADER_";
+
+export const SPARQL_BATCH_SIZE = env.get("SPARQL_BATCH_SIZE").default(0).asIntPositive();
+export const ENABLE_SPARQL_BATCHING = SPARQL_BATCH_SIZE > 0;
+
+export const SPARQL_AUTH_USER = env.get("SPARQL_AUTH_USER").asString();
+export const SPARQL_AUTH_PASSWORD = env.get("SPARQL_AUTH_PASSWORD").asString();
+
+export const BLANK_NODE_NAMESPACE = env.get("BLANK_NODE_NAMESPACE").default("http://mu.semte.ch/blank#").asString();
+export const CRON_PATTERN = env.get("CRON_PATTERN").default("* 0 * * * *").asString();
+
+const RUN_ONCE_VAR = env.get("RUN_ONCE").default("false").asBool();
+const RUNONCE_VAR = env.get("RUNONCE").default("false").asBool();
+export const RUN_ONCE = RUN_ONCE_VAR || RUNONCE_VAR;
+
+export const MU_APPLICATION_GRAPH = env.get("MU_APPLICATION_GRAPH").required().asString(); // Provided by template
+export const MU_SPARQL_ENDPOINT = env.get("MU_SPARQL_ENDPOINT").required().asString(); // Provided by template
+export const DEBUG_AUTH_HEADERS = env.get("DEBUG_AUTH_HEADERS").default("false").asBool();
+
+export const NODE_ENV = env.get("NODE_ENV").default("production").asEnum(["development", "production"]);
+
+export function logConfig() {
+  // Should this use the logger instead?
+  console.log("Config", {
+    INGEST_MODE,
+    REPLACE_VERSIONS,
+    PERSIST_STATE,
+    LDES_ENDPOINT_VIEW,
+    LDES_POLLING_INTERVAL,
+    LDES_REQUESTS_PER_MINUTE,
+    SPARQL_BATCH_SIZE,
+    SPARQL_AUTH_USER,
+    SPARQL_AUTH_PASSWORD,
+    BLANK_NODE_NAMESPACE,
+    CRON_PATTERN,
+    RUN_ONCE,
+    MU_APPLICATION_GRAPH,
+    MU_SPARQL_ENDPOINT,
+    DEBUG_AUTH_HEADERS,
+  });
+}
--- lib/sparql-queries.ts
+++ lib/sparql-queries.ts
@@ -1,7 +1,5 @@
 import * as RDF from "@rdfjs/types";
-import { TreeProperties, extractEndpointHeadersFromEnv, toString } from "./utils";
-import { DataFactory } from "n3";
-import { EXT } from "./namespaces";
+import { extractEndpointHeadersFromEnv, toString } from "./utils";
 import {
   MU_APPLICATION_GRAPH,
   SPARQL_AUTH_USER,
@@ -9,11 +7,9 @@ import {
   SPARQL_ENDPOINT_HEADER_PREFIX,
   SPARQL_BATCH_SIZE,
   ENABLE_SPARQL_BATCHING
-} from "./config";
-import { State } from "@treecg/actor-init-ldes-client";
+} from "../cfg";
 // @ts-ignore
 import { querySudo, updateSudo, ConnectionOptions } from "@lblod/mu-auth-sudo";
-const { quad, namedNode, variable, literal } = DataFactory;
 
 const SPARQL_ENDPOINT_HEADERS = extractEndpointHeadersFromEnv(SPARQL_ENDPOINT_HEADER_PREFIX);
 
@@ -122,85 +118,10 @@ export async function executeDeleteQuery (quads: RDF.Quad[]) {
   }
 }
 
-export async function getLatestTimestamp (baseResource: RDF.NamedNode, treeProperties: TreeProperties) {
-  const quads = [
-    quad(variable("version"), treeProperties.versionOfPath, baseResource),
-    quad(variable("version"), treeProperties.timestampPath, variable("timestamp"))
-  ];
-  const variables = [variable("timestamp")];
-  const sparqlQuery = constructSelectQuery(variables, quads, 'ORDER BY DESC(?timestamp)');
-  try {
-    const response = await query(sparqlQuery);
-    const timestamp = extractVariableFromResponse(response, "timestamp")?.shift();
-    if (timestamp) {
-      return timestamp;
-    }
-    return;
-  } catch (e) {
-    console.error(e);
-  }
-}
-
 export async function executeDeleteInsertQuery (
   quadsToDelete: RDF.Quad[],
   quadsToInsert: RDF.Quad[]
 ) {
   await executeDeleteQuery(quadsToDelete);
   await executeInsertQuery(quadsToInsert);
-}
-
-export async function fetchState (stream: RDF.NamedNode): Promise<State | undefined> {
-  const quads = [
-    quad(stream, EXT("state"), variable("state"))
-  ];
-  const variables = [variable("state")];
-  const sparqlQuery = constructSelectQuery(variables, quads);
-  try {
-    const response = await query(sparqlQuery);
-    const stateString = extractVariableFromResponse(response, "state")?.shift();
-    if (stateString) {
-      return JSON.parse(stateString);
-    }
-    return;
-  } catch (e) {
-    console.error(e);
-  }
-}
-
-function extractVariableFromResponse (
-  response: any,
-  variable: string
-): string[] | undefined {
-  const bindings = response.results.bindings;
-  if (bindings && bindings.length) {
-    return bindings.map((binding: any) => binding[variable].value);
-  }
-}
-
-export async function getVersion (resource: RDF.NamedNode, treeProperties: TreeProperties) {
-  const quads = [quad(variable("v"), treeProperties.versionOfPath, resource)];
-  const variables = [variable("v")];
-  const sparqlQuery = constructSelectQuery(variables, quads);
-
-  try {
-    const response = await query(sparqlQuery);
-    const versionUris = extractVariableFromResponse(response, "v");
-    if (versionUris) {
-      return namedNode(versionUris[0]);
-    }
-    return;
-  } catch (e) {
-    console.error(e);
-  }
-}
-
-export async function updateState (stream: RDF.NamedNode, state: State) {
-  const stateString = JSON.stringify(state);
-  const genericStateQuads = [
-    quad(stream, EXT("state"), variable("state"))
-  ];
-  const newStateQuads = [
-    quad(stream, EXT("state"), literal(stateString))
-  ];
-  await executeDeleteInsertQuery(genericStateQuads, newStateQuads);
 }
