ccc75bfe560268463e6a27b5377118fc06196629
piemonkey <piemonkey@protonmail.ch>
2025-09-25 20:53:43 +0200

    Add overridden fetch to sanitise feed contents
    Does not add a way to configure it or turn it off yet.

--- lib/fetch/custom-fetch.ts
+++ lib/fetch/custom-fetch.ts
@@ -1,23 +1,37 @@
 import { LDES_REQUESTS_PER_MINUTE } from "../../cfg";
 import { custom_headers_fetch } from "./custom-headers-fetch";
 import ThrottledFetch from "./throttled-fetch";
+import { StringReplaceTransformStream } from "string-replace-transform-stream";
 
+type FetchArgs = Parameters<typeof fetch>;
 
-let fetch_f: typeof fetch;
+// TODO add an env var for what to sanitise and whether to do it in the first place
+const sanitisedFetch = async (...args: FetchArgs) => {
+  const res = await fetch(...args);
+  const sanitisedBody = res.body?.pipeThrough(new StringReplaceTransformStream("\\u0020", ""));
+
+  return new Response(sanitisedBody, {
+    status: res.status,
+    statusText: res.statusText,
+    headers: res.headers
+  });
+};
+
+let fetchF: typeof fetch;
 if (LDES_REQUESTS_PER_MINUTE > 0) {
   const throttledFetch = new ThrottledFetch(LDES_REQUESTS_PER_MINUTE);
-  fetch_f = throttledFetch.throttled_fetch.bind(throttledFetch);
+  fetchF = (...args: FetchArgs) => throttledFetch.throttled_fetch(args[0], args[1], sanitisedFetch);
 } else {
-  fetch_f = fetch;
+  fetchF = sanitisedFetch;
 }
 
-export async function custom_fetch(
-  input: RequestInfo | URL,
-  init?: RequestInit,
+export async function custom_fetch (
+  input: FetchArgs[0],
+  init?: FetchArgs[1]
 ): Promise<Response> {
   return custom_headers_fetch(
     input,
     init,
-    fetch_f,
-  )
+    fetchF
+  );
 }
